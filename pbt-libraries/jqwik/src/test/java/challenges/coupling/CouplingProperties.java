598ad5f57e5d3d80aee9d1c06b6ab743d8fd416f
Johannes Link <business@johanneslink.net>
2020-08-22 12:49:59 +0200

    jqwik: Added coupling challenge code (no report yet)

---
+++ pbt-libraries/jqwik/src/test/java/challenges/coupling/CouplingProperties.java
@@ -0,0 +1,31 @@
+package challenges.coupling;
+
+import java.util.*;
+
+import challenges.*;
+
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.*;
+import net.jqwik.api.lifecycle.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+@AddLifecycleHook(ShrinkingStatistics.class)
+@PropertyDefaults(shrinking = ShrinkingMode.FULL, afterFailure = AfterFailureMode.RANDOM_SEED)
+public class CouplingProperties {
+
+	@Label("coupling")
+	@Property
+	void test(@ForAll List<@IntRange(min = 0, max = 10) Integer> ls) {
+		Assume.that(
+				ls.stream().allMatch(v -> v < ls.size())
+		);
+		for (int i = 0; i < ls.size(); i++) {
+			int j = ls.get(i);
+			if (i != j) {
+				assertThat(ls.get(j)).isNotEqualTo(i);
+			}
+		}
+	}
+
+}
