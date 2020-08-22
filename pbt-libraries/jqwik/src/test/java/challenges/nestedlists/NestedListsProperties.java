ce3c97a65875ee39f739ba1e22da73a1acb77288
Johannes Link <business@johanneslink.net>
2020-08-22 13:22:22 +0200

    jqwik: Added nestedlists challenge code (no report yet)

---
+++ pbt-libraries/jqwik/src/test/java/challenges/nestedlists/NestedListsProperties.java
@@ -0,0 +1,25 @@
+package challenges.nestedlists;
+
+import java.util.*;
+
+import challenges.*;
+
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.*;
+import net.jqwik.api.lifecycle.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+@AddLifecycleHook(ShrinkingStatistics.class)
+@PropertyDefaults(shrinking = ShrinkingMode.FULL, afterFailure = AfterFailureMode.RANDOM_SEED)
+public class NestedListsProperties {
+
+	@Label("nestedlists")
+	@Property
+	@Report(Reporting.FALSIFIED)
+	void test(@ForAll List<List<@IntRange(min = 0, max = 0) Integer>> ls) {
+		int sum = ls.stream().mapToInt(List::size).sum();
+		assertThat(sum).isLessThanOrEqualTo(10);
+	}
+
+}
