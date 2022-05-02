68b7c9e8b45f5aec6ddcbaf6b3dd024fe9707a36
Gerard Murphy <gjmurphy1@icloud.com>
2022-05-02 10:33:38 +0100

    Add alternate forms of the existing tests to the Java version of `DeletionTest` - these employ direct generation of relevant removal indices instead of filtering from a random pool.

--- pbt-libraries/americium/src/test/java/challenges/deletion/DeletionTest.java
+++ pbt-libraries/americium/src/test/java/challenges/deletion/DeletionTest.java
@@ -4,14 +4,13 @@ import com.google.common.collect.ImmutableList;
 import com.sageserpent.americium.java.Trials;
 import com.sageserpent.americium.java.TrialsApi;
 import com.sageserpent.americium.java.TrialsTest;
-import org.junit.jupiter.api.TestInstance;
+import cyclops.data.tuple.Tuple2;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DeletionTest {
 
     private final static TrialsApi api = Trials.api();
@@ -27,7 +26,13 @@ public class DeletionTest {
             .filter(candidateElementSet -> !candidateElementSet.isEmpty())
             .flatMap(choices -> api.choose(choices).immutableLists());
 
-    private final Trials<Integer> indices = api.integers(0, 10);
+    private final static Trials<Integer> indices = api.integers(0, 10);
+
+    // As an alternative, copy the Scala version of this test and drive the generation of removal indices
+    // based on the list size.
+    private final static Trials<Tuple2<ImmutableList<Integer>, Integer>> listAndIndexPairs =
+            lists.filter(list -> !list.isEmpty()).flatMap(list -> api.integers(0, list.size() - 1).map(index -> Tuple2.of(list, index)));
+
 
     @TrialsTest(trials = {"lists", "indices"}, casesLimit = 20)
     void testThatFails(ImmutableList<Integer> list, int indexOfElementToRemoveFromList) {
@@ -52,5 +57,26 @@ public class DeletionTest {
 
             assertThat(mutable).doesNotContain(itemForRemoval);
         });
+    }
+
+    @TrialsTest(trials = {"listAndIndexPairs"}, casesLimit = 20)
+    void testThatFailsInAlternateForm(ImmutableList<Integer> list, int indexOfElementToRemoveFromList) {
+        final List<Integer> mutable = list.stream().collect(Collectors.toList());
+
+        final int removedItem = mutable.remove(indexOfElementToRemoveFromList);
+
+        assertThat(mutable).doesNotContain(removedItem);
+    }
+
+    @TrialsTest(trials = {"listAndIndexPairs"}, casesLimit = 20)
+    void testThatSucceedsInAlternateForm(ImmutableList<Integer> list, int indexOfElementToRemoveFromList) {
+        final List<Integer> mutable = list.stream().collect(Collectors.toList());
+
+        final int itemForRemoval = mutable.get(indexOfElementToRemoveFromList);
+
+        while (mutable.remove((Object) itemForRemoval)) {
+        }
+
+        assertThat(mutable).doesNotContain(itemForRemoval);
     }
 }
