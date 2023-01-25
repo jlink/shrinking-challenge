e7d7978d33e89f02937ac681e41fbaa1564185d4
Gerard Murphy <gjmurphy1@icloud.com>
2023-01-25 18:09:49 +0000

    Use the Scala-specific overload of `CasesLimitStrategy.timed` added in Americium 1.8.2

--- pbt-libraries/americium/src/test/scala/bound5/Bound5Spec.scala
+++ pbt-libraries/americium/src/test/scala/bound5/Bound5Spec.scala
@@ -7,7 +7,9 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
-import java.time.Duration
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.FiniteDuration
+
 
 class Bound5Spec extends AnyFlatSpec with Matchers {
   private val api = Trials.api
@@ -31,7 +33,7 @@ class Bound5Spec extends AnyFlatSpec with Matchers {
   }
 
   "the sum" should "be nor more than five times the limit on the individual sums" in {
-    quintuples.withStrategy(caseSupplyCycle => CasesLimitStrategy.timed(Duration.ofSeconds(1 + caseSupplyCycle.numberOfPreviousShrinkages())), shrinkageStop = snoopOnShrinkage).supplyTo { quintuple =>
+    quintuples.withStrategy(caseSupplyCycle => CasesLimitStrategy.timed(FiniteDuration(1 + caseSupplyCycle.numberOfPreviousShrinkages(), TimeUnit.SECONDS)), shrinkageStop = snoopOnShrinkage).supplyTo { quintuple =>
       // Why does the original problem statement in Haskell insist on using a quintuple when it's much easier to use a sequence abstraction? Oh well...
       val sum: Short = quintuple.productIterator.asInstanceOf[Iterator[List[Short]]].flatten.sum
 
