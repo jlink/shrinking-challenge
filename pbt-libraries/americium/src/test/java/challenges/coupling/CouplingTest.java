af3c8fb3bc63c4cb1334fe2c487c32888d6b582e
Gerard Murphy <gjmurphy1@icloud.com>
2023-01-25 18:10:15 +0000

    Implement the Coupling challenge.

---
+++ pbt-libraries/americium/src/test/java/challenges/coupling/CouplingTest.java
@@ -0,0 +1,30 @@
+package challenges.coupling;
+
+import com.google.common.collect.ImmutableList;
+import com.sageserpent.americium.java.*;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CouplingTest {
+    private final static TrialsApi api = Trials.api();
+
+    private static final TrialsScaffolding.SupplyToSyntax<ImmutableList<Integer>> nonNegativeIntegers =
+            api.integers(0, 10).immutableLists().withStrategy((unused) -> CasesLimitStrategy.timed(Duration.ofSeconds(2)), TrialsScaffolding.OptionalLimits.defaults);
+
+    @ConfiguredTrialsTest("nonNegativeIntegers")
+    void testThatAListIsWellFormed(List<Integer> sut) {
+        final int size = sut.size();
+
+        Trials.whenever(sut.stream().allMatch(value -> size > value), () -> {
+            for (int index = 0; size > index; ++index){
+                final int secondaryIndex = sut.get(index);
+                if(index != secondaryIndex){
+                    assertThat(sut.get(secondaryIndex)).isNotEqualTo(index);
+                }
+            }
+        });
+    }
+}
