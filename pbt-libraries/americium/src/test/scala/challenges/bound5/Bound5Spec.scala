cc8230c3f45d131def9aefefc87357d44fc4a6c2
Gerard Murphy <gjmurphy1@icloud.com>
2023-01-26 15:08:37 +0000

    Updating links to reports - the reports are just cut and pasted placeholders.

--- pbt-libraries/americium/src/test/scala/challenges/bound5/Bound5Spec.scala
+++ pbt-libraries/americium/src/test/scala/challenges/bound5/Bound5Spec.scala
@@ -1,8 +1,10 @@
 package challenges.bound5
 
 import cats.implicits.catsSyntaxTuple5Semigroupal
+import challenges.bound5.Bound5Spec.{limit, quintuples}
 import com.sageserpent.americium.Trials
 import com.sageserpent.americium.java.CasesLimitStrategy
+import org.scalatest.BeforeAndAfter
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
@@ -10,7 +12,7 @@ import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
 
-class Bound5Spec extends AnyFlatSpec with Matchers {
+object Bound5Spec {
   private val api = Trials.api
 
   private val limit: Short = 256
@@ -29,16 +31,26 @@ class Bound5Spec extends AnyFlatSpec with Matchers {
       listsOfShortsWithLimitedSums,
       listsOfShortsWithLimitedSums
     ) mapN Tuple5.apply
+}
+
+class Bound5Spec extends AnyFlatSpec with Matchers with BeforeAndAfter {
+  private var testCaseCount: Integer = 0
 
   private val snoopOnShrinkage = () => {
     var failedCaseCounter = 0
 
     { caze: Any =>
-      println(s"$failedCaseCounter - Shrinkage has found a failing case: $caze")
+      println(
+        s"$failedCaseCounter - Shrinkage has found a failing case: $caze after $testCaseCount trials including this one."
+      )
       failedCaseCounter += 1
       false
     }
   }
+
+  before { testCaseCount = 0 }
+
+  after { println(s"Examined $testCaseCount cases in total.") }
 
   "the sum" should "be nor more than five times the limit on the individual sums" in {
     quintuples
@@ -53,6 +65,8 @@ class Bound5Spec extends AnyFlatSpec with Matchers {
         shrinkageStop = snoopOnShrinkage
       )
       .supplyTo { quintuple =>
+        testCaseCount += 1
+
         // Why does the original problem statement in Haskell insist on using a quintuple when it's much easier to use a sequence abstraction? Oh well...
         val sum: Short = quintuple.productIterator
           .asInstanceOf[Iterator[List[Short]]]
